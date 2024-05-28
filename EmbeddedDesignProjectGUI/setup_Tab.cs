67ac8684a218bc7cb83b31a854dd61c9c9f045c3
Diya Patel <nwy3203@autuni.ac.nz>
2024-05-28 19:23:11 +1200

    added individual classes for tabs and appboard class

---
+++ EmbeddedDesignProjectGUI/setup_Tab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Drawing;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace EmbeddedDesignProjectGUI
+{
+    public partial class Form1 : Form
+    {
+    }
+}
